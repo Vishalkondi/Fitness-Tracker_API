931764066a980a13aa9fd7e2f0d92497e7b16104
VISHAL KONDI <114770271+Vishalkondi@users.noreply.github.com>
2024-12-19 12:18:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import FitnessTracker from './FitnessTracker';
+
+function App() {
+  return (
+    <div className="App">
+      <FitnessTracker />
+    </div>
+  );
+}
+
+export default App;
+
